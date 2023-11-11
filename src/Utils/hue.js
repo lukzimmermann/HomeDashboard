460697b0c2b44983ee7df783757c78c88a29b4ad
Lukas Zimmermann <lukas@MacBook-Air-von-Lukas.local>
2023-11-12 00:34:50 +0100

    update api ip address

--- src/Utils/hue.js
+++ src/Utils/hue.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getRooms() {
-  const url = 'http://localhost:8000/hue/getLights/';
+  const url = 'http://192.168.1.130:8000/hue/getLights/';
 
   try {
     const response = await axios.get(url);
